42b8e5ae9fa17b1d4d452df52df6d4486a9dd1df
Robby <robby.azucena@gmail.com>
2020-04-20 11:38:14 +0800

    update

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,10 @@
 import '../css/main.scss';
+import '../css/font.scss';
 import 'jquery/dist/jquery.min.js';
 import 'popper.js/dist/esm/popper.min.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
 import 'bootstrap/scss/bootstrap.scss';
+
+import '../img/kast-logo.svg'
+import '../img/kast-logo-white.svg'
