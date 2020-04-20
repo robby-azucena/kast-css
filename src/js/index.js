f5ea0f720f96c19406050ae92d85027974721351
Robby <robby.azucena@gmail.com>
2020-04-20 11:49:18 +0800

    update

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,6 @@
 import '../css/main.scss';
 import '../css/font.scss';
+import '../css/button.scss';
 import 'jquery/dist/jquery.min.js';
 import 'popper.js/dist/esm/popper.min.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
