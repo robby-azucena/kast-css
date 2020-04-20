2c3cd0f7060da467fbde7b1ec69952e0654be795
Robby Azucena <32893045+robby-azucena@users.noreply.github.com>
2020-04-20 11:28:29 +0800

    Initial commit

---
+++ src/js/index.js
@@ -0,0 +1,6 @@
+import '../css/main.scss';
+import 'jquery/dist/jquery.min.js';
+import 'popper.js/dist/esm/popper.min.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
+import 'bootstrap/scss/bootstrap.scss';
