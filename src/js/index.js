197b142f669005d9b5751bfec86f1c0fa66d883c
Robby <robby.azucena@gmail.com>
2020-04-21 14:17:16 +0800

    update on navbar

--- src/js/index.js
+++ src/js/index.js
@@ -9,14 +9,28 @@ import 'bootstrap/scss/bootstrap.scss';
 
 import '../img/kast-logo.svg'
 import '../img/kast-logo-white.svg'
+import '../img/contact-active.svg'
+import '../img/contact-icn.svg'
+import '../img/dashboard-active.svg'
+import '../img/dashboard-icn.svg'
+import '../img/help-active.svg'
+import '../img/help-icn.svg'
+import '../img/inbox-active.svg'
+import '../img/inbox-icn.svg'
+import '../img/user-active.svg'
+import '../img/user-icn.svg'
 import '../img/kast-logo.png'
 import '../img/kast-logo-white.png'
+
+
+
 import '../favicons/favicon-16x16.png'
 
 
 $(document).ready(function () {
     $('.navbar-toggler').click(function (e) {
-        $(this).toggleClass('change')
-     })
+        $(this).toggleClass('change');
+        $('.nav-sidebar').toggleClass('show-sidebar');
+    })
 
 })
