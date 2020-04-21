b0874fb50049790bc05426aca4996ef01217d896
Robby <robby.azucena@gmail.com>
2020-04-21 17:23:47 +0800

    removed webpack automation

---
+++ docs/js/index.js
@@ -0,0 +1,10 @@
+
+
+
+$(document).ready(function () {
+    $('.navbar-toggler').click(function (e) {
+        $(this).toggleClass('change');
+        $('.nav-sidebar').toggleClass('show-sidebar');
+    })
+
+})
